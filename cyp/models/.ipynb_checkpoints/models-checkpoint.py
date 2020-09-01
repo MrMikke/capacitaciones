9ca1c17546c4b528f1347fcb1a64c3ff46f78174
RaulOsuna1 <osuna.adrian@hotmail.com>
2020-09-01 16:44:54 +0000

    creacion de la app cyp

---
+++ cyp/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class cyp(models.Model):
+    _name = 'cyp.cyp'
+    _description = 'Asasdasdsa'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(
+        compute="_value_pc", 
+        store=True
+    )
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
