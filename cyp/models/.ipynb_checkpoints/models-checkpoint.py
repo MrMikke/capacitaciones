5ede734271dda70abcd6a15f235ee3c652a9ed15
AleksBP <aleks.bep@hotmail.com>
2020-09-01 17:04:13 +0000

    Cambios

--- cyp/models/.ipynb_checkpoints/models-checkpoint.py
+++ cyp/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class cyp(models.Model):
     _name = 'cyp.cyp'
@@ -11,6 +11,12 @@ class cyp(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    
+    @api.constraints('value')
+    def _constraint_value(self):
+        for rec in self:
+            if rec.value==0;
+                raise ValidationError("NO PUEDE SER VALOR 0")
 
     @api.depends('value')
     def _value_pc(self):
