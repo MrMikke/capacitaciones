c9d186a539680e97daf7e19ef62b4e71dc9e6bf8
AleksBP <aleks.bep@hotmail.com>
2020-09-13 22:52:21 +0000

    Modelo peliculas v1

--- cvp/models/.ipynb_checkpoints/peliculas-checkpoint.py
+++ cvp/models/.ipynb_checkpoints/peliculas-checkpoint.py
@@ -7,12 +7,7 @@ class peliculas(models.Model):
     _name = 'cvp.peliculas'
     _description = 'Modelo para crear películas'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
+    imagen_pelicula = fields.Binary(
+        string='Imagen película',
+        attachment=False
+    )
